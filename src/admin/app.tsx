2b4b05118a55b282775109fdc88804b06d620a19
hakawa <hakawa.alves@gmail.com>
2024-02-29 15:04:49 -0300

    feat(home): finish strapi home config

--- src/admin/app.tsx
+++ src/admin/app.tsx
@@ -1,11 +1,10 @@
-import Icon from './extensions/icon.png'
-import Logo from './extensions/logo.svg'
-
+import Icon from "./extensions/icon.png";
+import Logo from "./extensions/logo.svg";
 
 export default {
   config: {
     auth: {
-      logo: Logo
+      logo: Logo,
     },
     head: {
       favicon: Icon,
@@ -13,26 +12,30 @@ export default {
     loacales: [],
     translations: {
       en: {
-        'Auth.form.welcome.title': 'Welcome to Won Games!',
-        'Auth.form.welcome.subtitle': 'Log in to your account',
-        'app.components.LeftMenu.navbrand.titel': 'Dashboard'
-      }
+        "Auth.form.welcome.title": "Welcome to Won Games!",
+        "Auth.form.welcome.subtitle": "Log in to your account",
+        "app.components.LeftMenu.navbrand.titel": "Dashboard",
+      },
     },
     menu: {
-      logo: Icon
+      logo: Icon,
     },
     theme: {
       light: {},
       dark: {
         colors: {
-          primary100: '#030415',
-          primary600: '#f231a5',
-          primary700: '#f231a5',
-          neutral0: '#0d102f',
-          neutral100: '#0d102f'
-        }
-      }
-    }
+          primary100: "#030415",
+          primary600: "#f231a5",
+          primary700: "#f231a5",
+          neutral0: "#0d102f",
+          neutral100: "#0d102f",
+        },
+      },
+    },
+    tutorials: false,
+    notifications: {
+      releases: false,
+    },
   },
   bootstrap() {},
 };
